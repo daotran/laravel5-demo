3d368e8c4387e71032babb57e9748e6adb65fb5e
Dennis <dennis@enclave.vn>
2015-10-19 17:55:54 +0700

    Update code

---
+++ resources/views/front/header.blade.php
@@ -0,0 +1,23 @@
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>{{ trans('front/site.title') }}</title>
+	<meta name="description" content="">	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- @yield('header') -->
+
+	{!! HTML::style('css/main_front.css') !!}
+
+	<!--[if (lt IE 9) & (!IEMobile)]>
+		{!! HTML::script('js/vendor/respond.min.js') !!}
+	<![endif]-->
+	<!--[if lt IE 9]>
+		{!! HTML::style('https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js') !!}
+		{!! HTML::style('https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js') !!}
+	<![endif]-->
+
+	{!! HTML::style('http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800') !!}
+	{!! HTML::style('http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic') !!}
+
+</head>
--- resources/views/front/template.blade.php
+++ resources/views/front/template.blade.php
@@ -4,103 +4,11 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 
-	<head>
-
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<title>{{ trans('front/site.title') }}</title>
-		<meta name="description" content="">	
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-
-		@yield('head')
-
-		{!! HTML::style('css/main_front.css') !!}
-
-		<!--[if (lt IE 9) & (!IEMobile)]>
-			{!! HTML::script('js/vendor/respond.min.js') !!}
-		<![endif]-->
-		<!--[if lt IE 9]>
-			{!! HTML::style('https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js') !!}
-			{!! HTML::style('https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js') !!}
-		<![endif]-->
-
-		{!! HTML::style('http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800') !!}
-		{!! HTML::style('http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic') !!}
-
-	</head>
+	@include('front.header')
 
   <body>
 
-	<!--[if lt IE 8]>
-		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-	<![endif]-->
-
-	<header role="banner">
-
-		<div class="brand">{{ trans('front/site.title') }}</div>
-		<div class="address-bar">{{ trans('front/site.sub-title') }}</div>
-		<div id="flags" class="text-center"></div>
-		<nav class="navbar navbar-default" role="navigation">
-			<div class="container">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="index.html">{{ trans('front/site.title') }}</a>
-				</div>
-				<div class="collapse navbar-collapse">
-					<ul class="nav navbar-nav">
-						<li {!! classActivePath('/') !!}>
-							{!! link_to('/', trans('front/site.home')) !!}
-						</li>
-						@if(session('statut') == 'visitor' || session('statut') == 'user')
-							<li {!! classActivePath('contact/create') !!}>
-								{!! link_to('contact/create', trans('front/site.contact')) !!}
-							</li>
-						@endif
-						<li {!! classActiveSegment(1, ['articles', 'blog']) !!}>
-							{!! link_to('articles', trans('front/site.blog')) !!}
-						</li>
-						@if(Request::is('auth/register'))
-							<li class="active">
-								{!! link_to('auth/register', trans('front/site.register')) !!}
-							</li>
-						@elseif(Request::is('password/email'))
-							<li class="active">
-								{!! link_to('password/email', trans('front/site.forget-password')) !!}
-							</li>
-						@else
-							@if(session('statut') == 'visitor')
-								<li {!! classActivePath('auth/login') !!}>
-									{!! link_to('auth/login', trans('front/site.connection')) !!}
-								</li>
-							@else
-								@if(session('statut') == 'admin')
-									<li>
-										{!! link_to_route('admin', trans('front/site.administration')) !!}
-									</li>
-								@elseif(session('statut') == 'redac') 
-									<li>
-										{!! link_to('blog', trans('front/site.redaction')) !!}
-									</li>
-								@endif
-								<li>
-									{!! link_to('auth/logout', trans('front/site.logout')) !!}
-								</li>
-							@endif
-						@endif
-						<li class="imgflag">
-							<a href="{!! url('language') !!}"><img width="32" height="32" alt="en" src="{!! asset('img/' . (session('locale') == 'fr' ? 'english' : 'french') . '-flag.png') !!}"></a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-		@yield('header')	
-	</header>
+	@include('front.banner')
 
 	<main role="main" class="container">
 		@if(session()->has('ok'))
@@ -112,10 +20,7 @@
 		@yield('main')
 	</main>
 
-	<footer role="contentinfo">
-		 @yield('footer')
-		<p class="text-center"><small>Copyright &copy; Momo</small></p>
-	</footer>
+	@include('front.footer')
 		
 	{!! HTML::script('//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js') !!}
 	<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
