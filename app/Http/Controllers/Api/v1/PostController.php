c22909c7c1f145965f1cdeb63eea26897413930f
Dennis <dennis@enclave.vn>
2016-01-22 16:08:30 +0700

    Update Restful API Post

--- app/Http/Controllers/Api/v1/PostController.php
+++ app/Http/Controllers/Api/v1/PostController.php
@@ -24,12 +24,50 @@ class PostController extends Controller {
 
         $user_id = Authorizer::getResourceOwnerId(); // the token user_id
         $user = User::find($user_id); // get the user data from database
-        
         // get all posts
         //$posts = $user->posts()->get(); //OR as below
         $posts = Post::where('user_id', '=', $user->id)->get();
 
         return Response::json($posts);
+    }
+
+    /**
+     * Store a newly created post in storage.
+     *
+     * @return Response
+     */
+    public function create() {
+        $status = false;
+
+        $post = new Post;
+        $user_id = Authorizer::getResourceOwnerId(); // the token user_id
+
+        $post->title = "Post 512a";
+        $post->slug = "post-512a";
+        $post->content = "Content of Post 5";
+        $post->seen = 0;
+        $post->active = 1;
+        $post->user_id = $user_id;
+
+        // $post->title = Request::get('url');
+        //$url->description = Request::get('description');
+        // Validation and Filtering is sorely needed!!
+        // Seriously, I'm a bad person for leaving that out.
+        if ($post->save()) {
+            $status = true;
+        }
+
+        if ($status) {
+            return Response::json(array(
+                        'success' => TRUE,
+                        'data' => $post->toArray()), 200
+            );
+        } else {
+            return Response::json(array(
+                        'success' => FALSE,
+                        'message' => 'Sorry, create new post unsuccessfully!'
+            ));
+        }
     }
 
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -77,6 +77,9 @@ Route::controllers([
  */
 
 // Generate the token after passing the username and password
+// API: http://localhost/laravel5-demo/public/oauth/access_token
+// config form-data/x-www-form-urlencode
+// {"grant_type":"password", "client_id":"f3d259ddd3ed8ff3843839b", "client_secret":"4c7f6f8fa93d59c45502c0ae8c4a95b", "username":"testuser", "password":"password"}
 Route::post('oauth/access_token', function() {
     return Response::json(Authorizer::issueAccessToken());
 });
@@ -84,8 +87,8 @@ Route::post('oauth/access_token', function() {
 // Create a new user for testing
 Route::get('register',function(){
     $user = new App\Models\User();
-    $user->username = "test user";
-    $user->email = "test@test.com";
+    $user->username = "noland";
+    $user->email = "noland@test.com";
     $user->password = \Illuminate\Support\Facades\Hash::make("password");
     $user->role_id = 1;
     $user->save(); 
@@ -98,10 +101,18 @@ Route::get('register',function(){
 // protected all the routes inside it with oauth (‘before’ => ‘oauth’)
 Route::group(['prefix' => 'api/v1', 'before' => 'oauth'], function () {
     // get the token owner and retrieve the user info
+    // API: http://localhost/laravel5-demo/public/api/v1/user_token?access_token=4dOH0eDAOTzVHDVZ8v9r97zz9CnXAmVD40rjhqzL
     Route::get('/user_token', 'Api\v1\UserController@getUserInfo');
     
     // get all posts of token user_id
+    // API: http://localhost/laravel5-demo/public/api/v1/posts?access_token=IBzXJ9wXQAdQH1UiCjxK1A9JPIieTtEMbqerjmdF
     Route::get('/posts',  'Api\v1\PostController@getAllPosts');
+    
+    // create a new post
+    // API: http://localhost/laravel5-demo/public/api/v1/posts/create
+    // config form-data/x-www-form-urlencode
+    // access_token=IBzXJ9wXQAdQH1UiCjxK1A9JPIieTtEMbqerjmdF
+    Route::post('posts/create', 'Api\v1\PostController@create');
     
 });
 
@@ -110,32 +121,18 @@ Route::group(['prefix' => 'api/v1', 'before' => 'oauth'], function () {
 //Route::get('admin/users', function(){
 //    $data = Input::get('data');
 //});
-//Route::get('admin/users' , array('before' => 'ajax:data', 'as' => 'admin.users', 'uses' => 'UserController@dataRefresh'));
-//Route::filter('ajax', function($route, $request, $param){
-//
-//    // This will give query string 'refresh'
-//    // if you passed it as http://domain.com?data=refresh
-//    $data = $request->get($param);
-//
-//    // You can retrieve the $param, third argument
-//    // if you pass a parameter, i.e. 'refresh'
-//    // param will contain 'refresh'
-//});
+Route::get('admin/users' , array('before' => 'ajax:data', 'as' => 'admin.users', 'uses' => 'UserController@dataRefresh'));
+Route::filter('ajax', function($route, $request, $param){
 
+    // This will give query string 'refresh'
+    // if you passed it as http://domain.com?data=refresh
+    $data = $request->get($param);
 
-//Route::get('oauth/authorize', ['as' => 'oauth.authorize.get', 'middleware' => ['check-authorization-params', 'auth'], function() {
-//   $authParams = \LucaDegasperi\OAuth2Server\Authorizer::getAuthCodeRequestParams();
-//
-//   $formParams = array_except($authParams,'client');
-//
-//   $formParams['client_id'] = $authParams['client']->getId();
-//
-//   $formParams['scope'] = implode(config('oauth2.scope_delimiter'), array_map(function ($scope) {
-//       return $scope->getId();
-//   }, $authParams['scopes']));
-//
-//   return \Illuminate\View\View::make('oauth.authorization-form', ['params' => $formParams, 'client' => $authParams['client']]);
-//}]);
+    return Response::json($data);
+    // You can retrieve the $param, third argument
+    // if you pass a parameter, i.e. 'refresh'
+    // param will contain 'refresh'
+});
 
 
 
